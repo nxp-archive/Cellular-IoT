80afe9e25098e5ba3f5db126b83737e52af8f7aa
nxf37702 <alexandre.moulinet@nxp.com>
2020-12-02 18:53:07 +0100

    [SEPROD-95] Update the project to use Shared Access Signature (SAS) authentication - Update msft_Azure_IoT_clientcredential_keys.h to default usage

---
+++ msft_Azure_IoT/demo/msft_Azure_IoT_clientcredential_keys.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+
+#ifndef MSFT_AZURE_IOT_CLIENTCREDENTIAL_KEYS_H_
+#define MSFT_AZURE_IOT_CLIENTCREDENTIAL_KEYS_H_
+
+#ifdef SAS_KEY
+
+#define keyDEVICE_SAS_PRIMARY_KEY ""
+#define keyCLIENT_CERTIFICATE_PEM NULL
+#define keyCLIENT_PRIVATE_KEY_PEM NULL
+
+#else
+
+/*
+ * @brief PEM-encoded client certificate.
+ *
+ * @todo If you are running one of the FreeRTOS demo projects, set this
+ * to the certificate that will be used for TLS client authentication.
+ *
+ * @note Must include the PEM header and footer:
+ * "-----BEGIN CERTIFICATE-----\n"\
+ * "...base64 data...\n"\
+ * "-----END CERTIFICATE-----\n"
+ */
+#define keyCLIENT_CERTIFICATE_PEM                   ""
+/*
+ * @brief PEM-encoded client private key.
+ *
+ * @todo If you are running one of the FreeRTOS demo projects, set this
+ * to the private key that will be used for TLS client authentication.
+ *
+ * @note Must include the PEM header and footer:
+ * "-----BEGIN RSA PRIVATE KEY-----\n"\
+ * "...base64 data...\n"\
+ * "-----END RSA PRIVATE KEY-----\n"
+ */
+#define keyCLIENT_PRIVATE_KEY_PEM                   ""
+
+#endif
+#endif /* MSFT_AZURE_IOT_CLIENTCREDENTIAL_KEYS_H_ */
